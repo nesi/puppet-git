34c53b4d852a5a39032134eddfc8e9c9f9bdb44a
Aaron Hicks <Aethylred@gmail.com>
2012-08-28 11:29:48 +1200

    Rework powershell script to update all the files from templates with .orig suffix.

--- setauthor.ps1
+++
@@ -1,8 +0,0 @@
-Param($author)
-if ($author -match "^\w+$") {
-	echo "Replacing author with $author..."
-	Get-Content Modulefile.orig | Foreach-Object {$_ -replace "aethylred", $author} | Set-Content Modulefile
-	echo "New Modulefile created"
-} else {
-  echo "Bad author '$author', must be a single word made up of word caracters [a-zA-Z_0-9]"
-}
