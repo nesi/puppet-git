6d9614a2449e6ff486d85f5cb1ee7424308f83a2
newLoki <newloki91@googlemail.com>
2012-10-23 09:16:34 +0200

    add debian git package name to params, this should work with debian

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,7 @@
 class git::params {
 
   case $operatingsystem {
-  	'CentOS','Ubuntu':{
+  	'CentOS','Ubuntu', 'Debian' :{
   		$package 			= 'git'
   		$svn_package	= 'git-svn'
   		$gui_package	= 'git-gui'
