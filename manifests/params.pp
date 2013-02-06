c5bc1e0abe9dd1754480bb95e404be8ec7c9cbf3
Lance Speelmon <lance@speelmon.com>
2013-02-05 19:04:55 -0700

    Add support for Amazon Linux in git::params

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,7 @@
 class git::params {
 
   case $operatingsystem {
-  	'CentOS','Ubuntu', 'Debian' :{
+  	'CentOS','Ubuntu', 'Debian', 'Amazon' :{
   		$package 			= 'git'
   		$svn_package	= 'git-svn'
   		$gui_package	= 'git-gui'
