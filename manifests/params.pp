a72bd2717a546f7ee53fdfab16e048c89213a206
Adam Jahnke <adamyonk@me.com>
2013-03-03 16:31:34 -0600

    Updates the $operatingsystem case
    Fixes #5

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,13 +5,19 @@
 class git::params {
 
   case $operatingsystem {
-    'CentOS','Ubuntu', 'Debian' :{
+    'CentOS', 'Debian': {
       $package      = 'git'
       $svn_package  = 'git-svn'
       $gui_package  = 'git-gui'
       $bin          = '/usr/bin/git'
     }
-    default:{
+    'Ubuntu': {
+      $package      = 'git-core'
+      $svn_package  = 'git-svn'
+      $gui_package  = 'git-gui'
+      $bin          = '/usr/bin/git'
+    }
+    default: {
       warning("git not configured for ${operatingsystem} on ${fqdn}")
     }
   }
